df0f8d60298568ebce795c69a0e026b6f2895677
yao-le <le.yao@northeastern.edu>
2023-02-15 17:38:22 -0500

    a5 JavaScript 2023 spring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import Labs from "./labs";
-import HelloWorld from "./labs/a6/hello-world";
-import Tuiter from "./tuiter";
-import {BrowserRouter} from "react-router-dom";
+// import Labs from "./labs";
+// import HelloWorld from "./labs/a6/hello-world";
+// import Tuiter from "./tuiter";
+// import {BrowserRouter} from "react-router-dom";
 // import {Routes, Route} from "react-router-dom";
 //
 // function App() {
