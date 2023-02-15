9715146d4db7f5facb075a74e850e4614ad7d335
yao-le <le.yao@northeastern.edu>
2023-02-15 16:54:21 -0500

    a5 JavaScript 2023 spring

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import Labs from "./labs";
 import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
 import {BrowserRouter} from "react-router-dom";
-import {Routes, Route} from "react-router-dom";
-
-function App() {
-    return (
-        <BrowserRouter>
-            <div className="container">
-                <Routes>
-                    <Route index
-                           element={<Labs/>}/>
-                    <Route path="/hello"
-                           element={<HelloWorld/>}/>
-                    <Route path="/tuiter"
-                           element={<Tuiter/>}/>
-                </Routes>
-            </div>
-        </BrowserRouter>
-    );
-}
-
-export default App;
+// import {Routes, Route} from "react-router-dom";
+//
+// function App() {
+//     return (
+//         <BrowserRouter>
+//             <div className="container">
+//                 <Routes>
+//                     <Route index
+//                            element={<Labs/>}/>
+//                     <Route path="/hello"
+//                            element={<HelloWorld/>}/>
+//                     <Route path="/tuiter"
+//                            element={<Tuiter/>}/>
+//                 </Routes>
+//             </div>
+//         </BrowserRouter>
+//     );
+// }
+//
+// export default App;
--- src/tuiter/who-to-follow-list/who-to-follow-list-item.js
+++ src/tuiter/who-to-follow-list/who-to-follow-list-item.js
@@ -1,33 +1,33 @@
-import React from "react";
-
-const WhoToFollowListItem = (
-    {
-        who = { userName: 'NASA', handle: 'NASA', avatarIcon: 'nasa-icon.jpg' }
-    }
-) => {
-    return (
-        <div className="list-group-item">
-        <div className="d-flex flex-row align-items-center py-2">
-            <div className="w-20 me-3">
-                <img src={`/images/${who.avatarIcon}`} className="rounded-circle" width="48px" height="48px" alt="" />
-            </div>
-
-            <div className="w-60">
-                <div className="mb-0">
-                    <span className="fw-bold">{who.userName}</span>
-                    <i className="fas fa-check-circle me-1"></i>
-                </div>
-                <span className="mb-0">@{who.handle}</span>
-            </div>
-
-            <div className="ms-auto">
-                <button className="btn btn-primary text-white text-md rounded-pill w-100 px-3">
-                    Follow
-                </button>
-            </div>
-        </div>
-    </div>
-    );
-}
-
-export default WhoToFollowListItem;
+// import React from "react";
+//
+// const WhoToFollowListItem = (
+//     {
+//         who = { userName: 'NASA', handle: 'NASA', avatarIcon: 'nasa-icon.jpg' }
+//     }
+// ) => {
+//     return (
+//         <div className="list-group-item">
+//         <div className="d-flex flex-row align-items-center py-2">
+//             <div className="w-20 me-3">
+//                 <img src={`/images/${who.avatarIcon}`} className="rounded-circle" width="48px" height="48px" alt="" />
+//             </div>
+//
+//             <div className="w-60">
+//                 <div className="mb-0">
+//                     <span className="fw-bold">{who.userName}</span>
+//                     <i className="fas fa-check-circle me-1"></i>
+//                 </div>
+//                 <span className="mb-0">@{who.handle}</span>
+//             </div>
+//
+//             <div className="ms-auto">
+//                 <button className="btn btn-primary text-white text-md rounded-pill w-100 px-3">
+//                     Follow
+//                 </button>
+//             </div>
+//         </div>
+//     </div>
+//     );
+// }
+//
+// export default WhoToFollowListItem;
